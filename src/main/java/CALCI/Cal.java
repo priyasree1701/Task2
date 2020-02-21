34fca76cdf962d8242d0ad7714f04fb1e91439cd
priyasree <priyasree.manthri@gmail.com>
2020-02-21 19:46:44 +0530

    first commit

---
+++ src/main/java/CALCI/Cal.java
@@ -0,0 +1,46 @@
+package CALCI;
+import java.util.*;
+
+public class Cal{
+		 
+		 public static void main(String[] args) 
+		 {
+			 int a, b, res;
+			 char ch;
+			 Scanner scan=new Scanner(System.in);
+			 
+			 System.out.print("Enter First Number : ");
+			 a=scan.nextInt();
+			 System.out.print("Enter Second Number : ");
+			 b=scan.nextInt();
+			 System.out.print("Enter Operator (+,-,*,/) : ");
+			 ch=scan.next().charAt(0);
+			 
+			 if(ch == '+')
+			 {
+				 res = a + b;
+				 System.out.print("Result = " +res);
+			 }
+			 else if(ch == '+')
+			 {
+				 res = a - b;
+				 System.out.print("Result = " +res); 
+			 }
+			 else if(ch == '*')
+			 {
+				 res = a * b;
+				 System.out.print("Result = " +res);
+			 }
+			 else if(ch == '/')
+			 {
+				 res = a / b;
+				 System.out.println("Result = " +res);
+			 }
+			 else 
+			 {
+				 System.out.println("Wrong Operator !!!");
+			 }
+		 }
+		
+
+	}
